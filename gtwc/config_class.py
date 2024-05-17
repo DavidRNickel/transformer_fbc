d31502ff3704190f57fe4a3d70c26ac5accca593
DavidRNickel <dnickel@purdue.edu>
2024-05-17 17:00:34 -0400

    belief working; starting gtwc

--- gtwc/config_class.py
+++ gtwc/config_class.py
@@ -7,10 +7,17 @@ import torch
 
 class Config():
     def __init__(self):
+        self.use_tensorboard = True
+        self.use_belief_network = False
+
         # settings for communications-related stuff
-        self.K = 3 # length of bitstream
-        self.N = 9 # number channel usees
-        self.knowledge_vec_len = self.K + 2*(self.N - 1) # all old bits and all feedback
+        self.K = 51 # length of bitstream
+        self.M = 3 # length of shorter block
+        assert(self.K % self.M == 0)
+        self.N = self.M * self.K 
+        self.knowledge_vec_len = self.M + 2*(self.M * self.N//self.K - 1) # all old bits and all feedback
+        if self.use_belief_network:
+            self.knowledge_vec_len += 2*self.M
         self.snr_ff = 1 # in dB
         self.snr_fb = 20 # in dB
         self.noise_pwr_ff = 10**(-self.snr_ff/10)
@@ -18,9 +25,9 @@ class Config():
 
         # Model settings
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        print(f'device: {self.device}')
         self.max_len_enc = self.N
         self.num_layers_xmit = 2 
+        self.num_layers_belief = 2
         self.num_layers_recv = 3
         self.n_heads = 1
         self.d_model = 32
@@ -32,9 +39,8 @@ class Config():
         self.num_epochs = 100
         self.batch_size = 2500
         self.num_training_samps = int(1E6)
+        self.num_valid_samps = int(1e5)
+        assert(self.num_training_samps % self.batch_size == 0)
+        assert(self.num_valid_samps % self.batch_size == 0)
         self.num_iters_per_epoch = self.num_training_samps // self.batch_size
         self.grad_clip = .5
-        self.num_valid_samps = int(1e5)
-        self.pooling_type = 'first'
-        self.use_tensorboard = False
-        self.use_belief_network = True
--- gtwc/xformer_main.py
+++ gtwc/xformer_main.py
@@ -23,18 +23,19 @@ def test_model(test_data, model, conf):
     pwr_avg = np.zeros((batch_size, conf.N))
     with torch.no_grad():
         for i in range(num_iters):
-            bits = test_data['bits'][batch_size*i : batch_size*(i+1)].unsqueeze(1).to(device)
-            output = model(bits)
-            bit_estimates = model.one_hot_to_bits(output).detach().clone().cpu().numpy().astype(np.bool_)
-            ber_tmp, bler_tmp = model.calc_error_rates(bit_estimates, bits.squeeze(1).detach().clone().cpu().numpy().astype(np.bool_))
+            bits = test_data['bits'][batch_size*i : batch_size*(i+1)].to(device)
+            b = bits.view(batch_size,-1,conf.M)
+            output = model(b).view(bs*model.num_blocks, 2**conf.M)
+            bit_estimates = model.one_hot_to_bits(output).bool().view(batch_size,-1).detach().clone().cpu().numpy().astype(np.bool_)
+            ber_tmp, bler_tmp = model.calc_error_rates(bit_estimates, bits.detach().clone().cpu().numpy().astype(np.bool_))
 
             ber += ber_tmp
             bler += bler_tmp
-            pwr_avg += np.array(model.transmit_power_tracking).T
+            # pwr_avg += np.array(model.transmit_power_tracking).T
 
         ber /= num_iters
         bler /= num_iters
-        pwr_avg /= num_iters
+        # pwr_avg /= num_iters
     
     return ber, bler, pwr_avg 
 
@@ -55,9 +56,11 @@ if __name__=='__main__':
             writer = SummaryWriter()
         writer = SummaryWriter()
 
-    bitstreams_train = torch.randint(0,2,(conf.num_training_samps, 1, conf.K)).to(device)
-    noise_ff_train = np.sqrt(conf.noise_pwr_ff) * torch.randn((conf.num_training_samps, conf.N))
-    noise_fb_train = np.sqrt(conf.noise_pwr_fb) * torch.randn((conf.num_training_samps, conf.N))
+    bs = conf.batch_size
+    bitstreams_train = torch.randint(0,2,(conf.num_training_samps, conf.K)).to(device)
+    # noise_ff_train = np.sqrt(conf.noise_pwr_ff) * torch.randn((conf.num_training_samps * conf.N)).view(conf.num_training_samps, -1, conf.N // conf.M)
+    # noise_fb_train = np.sqrt(conf.noise_pwr_fb) * torch.randn((conf.num_training_samps * conf.N)).view(conf.num_training_samps, -1, conf.N // conf.M)
+    # print(noise_ff_train.shape);sys.exit()
 
     n_valid_samps = conf.num_valid_samps
     n_valid_iters = n_valid_samps // conf.batch_size
@@ -75,18 +78,18 @@ if __name__=='__main__':
     bit_errors = []
     block_errors = []
     ctr = 0
-    bs = conf.batch_size
     for epoch in range(num_epochs):
         fbc.train()
         losses = []
         for i in range(conf.num_iters_per_epoch):
-            bitstreams = bitstreams_train[bs*i:bs*(i+1)].to(device)
-            noise_ff = noise_ff_train[bs*i:bs*(i+1)].to(device)
-            noise_fb = noise_fb_train[bs*i:bs*(i+1)].to(device)
+            bitstreams = bitstreams_train[bs*i:bs*(i+1)].int().to(device)
+            # noise_ff = noise_ff_train[bs*i:bs*(i+1)].to(device)
+            # noise_fb = noise_fb_train[bs*i:bs*(i+1)].to(device)
 
             optimizer.zero_grad()
-            output = fbc(bitstreams, noise_ff, noise_fb)
-            b = bitstreams.int().permute(0,2,1).squeeze(-1)
+            # output = fbc(bitstreams.view(bs,-1,conf.M), noise_ff, noise_fb)
+            b = bitstreams.view(bs,-1,conf.M)
+            output = fbc(b).view(bs*fbc.num_blocks, 2**fbc.M)
             b_one_hot = fbc.bits_to_one_hot(b).float()
             loss = loss_fn(output, b_one_hot)
 
@@ -95,8 +98,8 @@ if __name__=='__main__':
             losses.append(L:=loss.item())
             optimizer.step()
 
-            bit_estimates = fbc.one_hot_to_bits(output).bool()
-            ber, bler = fbc.calc_error_rates(bit_estimates, bitstreams.squeeze(1).bool())
+            bit_estimates = fbc.one_hot_to_bits(output).bool().view(bs,-1)
+            ber, bler = fbc.calc_error_rates(bit_estimates, bitstreams.bool())
 
             if i % 100 == 0:
                 print(f'Epoch (iter): {epoch} ({i}), Loss: {loss.item()}')
